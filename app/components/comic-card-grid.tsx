88e1c94141962c5fbd9b0aca0b64042cef06a531
roggc <roggc9@gmail.com>
2025-02-11 19:23:31 +0100

    sort comics

--- app/components/comic-card-grid.tsx
+++ app/components/comic-card-grid.tsx
@@ -2,15 +2,13 @@ import ComicCard from './comic-card'
 import { Comic } from '@/app/types/marvel/comic'
 
 type ComicCardGridProps = {
-    data: {
-        results: Comic[]
-    }
+    results: Comic[]
 }
 
-export default function ComicCardGrid({ data }: ComicCardGridProps) {
+export default function ComicCardGrid({ results }: ComicCardGridProps) {
     return (
         <div className="flex gap-4 overflow-x-auto px-4 pb-4">
-            {data.results.map((comic: Comic) => (
+            {results.map((comic: Comic) => (
                 <ComicCard key={comic.id} comic={comic} />
             ))}
         </div>
--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getMarvelData } from '@/app/lib/marvel-api'
 import Header from '@/app/components/header'
 import DetailCard from '@/app/components/detail-card/detail-card'
 import ComicCardGrid from '@/app/components/comic-card-grid'
+import { Comic } from '@/app/types/marvel/comic'
 
 const LIMIT = 20
 
@@ -26,7 +27,25 @@ export default async function Detail({
                 id={id}
             />
             <div className="font-bold text-2xl px-4 pt-10 pb-5">COMICS</div>
-            <ComicCardGrid data={data} />
+            <ComicCardGrid
+                results={data.results.sort((c1: Comic, c2: Comic) => {
+                    const date1Str =
+                        c1.dates.find((date) => date.type === 'onsaleDate')
+                            ?.date ?? ''
+                    const date2Str =
+                        c2.dates.find((date) => date.type === 'onsaleDate')
+                            ?.date ?? ''
+                    const time1 = new Date(date1Str).getTime()
+                    const time2 = new Date(date2Str).getTime()
+
+                    // Manejo de fechas inválidas
+                    if (isNaN(time1) && isNaN(time2)) return 0
+                    if (isNaN(time1)) return 1
+                    if (isNaN(time2)) return -1
+
+                    return time2 - time1
+                })}
+            />
         </>
     )
 }
