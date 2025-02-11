0d152c05b461fd5fe08fd81c66f883b8aae3adf4
roggc <roggc9@gmail.com>
2025-02-11 10:39:37 +0100

    hearts

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,14 +1,11 @@
-import Image from 'next/image'
+import Logo from './logo'
+import Favourites from './favourites'
 
 export default function Header() {
     return (
-        <div className="p-4 bg-black sticky top-0 z-10">
-            <Image
-                src="/Marvel logo.png" // La ruta relativa desde la carpeta public
-                alt="Marvel logo" // Texto alternativo para la imagen
-                width={130} // Especifica el ancho en píxeles
-                height={52} // Especifica la altura en píxeles
-            />
+        <div className="p-4 bg-black sticky top-0 z-10 flex justify-between items-center">
+            <Logo />
+            <Favourites />
         </div>
     )
 }
