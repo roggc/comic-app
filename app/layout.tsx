1891a4d6d0f041b2cbbc36f543a6abdad14ac46d
roggc <roggc9@gmail.com>
2025-02-12 11:11:45 +0100

    fix error in mobile?

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${robotoCondensed.variable} ${geistSans.variable} ${geistMono.variable}  antialiased`}
+                className={`${robotoCondensed.variable} ${geistSans.variable} ${geistMono.variable}  antialiased bg-white text-black`}
             >
                 {children}
                 <NProgressClient />
